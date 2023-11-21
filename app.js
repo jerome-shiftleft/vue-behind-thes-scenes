86ea4a24ff31c2bc16792dd74620c9d629243bf1
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-22 03:43:16 +0800

    ref usage

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      //console.dir(this.$refs.userText);
+      this.message = this.$refs.userText.value;
     },
   },
 });
